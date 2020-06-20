f7f9e981d8a682529fdf6b916e5906fa78475223
Adi Skop <adiskop@gmail.com>
2020-06-20 13:24:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,11 @@
-def display_board(board)
-
+def turn (board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
